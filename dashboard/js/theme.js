66ab35b5214cf73d0e8a73fa5750b76f966ab7ff
easye35 <easye35@gmail.com>
2026-04-03 11:49:33 -0600

    Delete dashboard directory

--- dashboard/js/theme.js
+++
@@ -1,6 +0,0 @@
-let theme = 'dark';
-
-function toggleTheme() {
-  theme = theme === 'dark' ? 'light' : 'dark';
-  document.body.className = theme;
-}
--- dashboard/js/utils.js
+++
@@ -1,3 +0,0 @@
-function restartContainer(name) {
-  alert("Restart request for container: " + name + "\n\nWire this to a backend endpoint when ready.");
-}
