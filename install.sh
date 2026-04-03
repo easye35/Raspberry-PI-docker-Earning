dfbec3714e58a1effb89fead43d6627fb6342db6
easye35 <easye35@gmail.com>
2026-04-02 21:39:07 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LIB_DIR="${ROOT_DIR}/lib"
 MODULE_DIR="${ROOT_DIR}/modules"
 
+# Export library paths for all modules
+export LOG_LIB="${LIB_DIR}/logging.sh"
+export SYS_LIB="${LIB_DIR}/system.sh"
+export DOCKER_LIB="${LIB_DIR}/docker.sh"
+
 # ---------------------------------------------------------
 # Load core libraries FIRST (logging must exist before use)
 # ---------------------------------------------------------
