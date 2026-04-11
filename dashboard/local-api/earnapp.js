bfcbd0b3b2050c98b1c88250fab0b89ad43e07ff
easye35 <easye35@gmail.com>
2026-04-11 13:17:36 -0600

    Delete dashboard/local-api/earnapp.js

--- dashboard/local-api/earnapp.js
+++
@@ -1,8 +0,0 @@
-async function fetchEarnApp() {
-    try {
-        const res = await fetch("http://earning-api:3001/api/earnapp/status");
-        return await res.json();
-    } catch (err) {
-        return { ok: false, error: err.toString() };
-    }
-}
